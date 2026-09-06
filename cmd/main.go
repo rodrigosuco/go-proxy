0905cafa2f2ae57c1360342157e1e8b65ea3d1b0
rodrigosuco <rod1dev@proton.me>
2026-09-06 17:26:35 -0300

    feat: add custom headers and set foward

--- cmd/main.go
+++ cmd/main.go
@@ -9,16 +9,17 @@ import (
 )
 
 func main() {
-	target, err := url.Parse("http://localhost:3000")
+	target, err := url.Parse("http://localhost:3000/api/v1")
 	if err != nil {
 		log.Fatal("Error parsing target URL: ", err)
 	}
 
 	proxy := &httputil.ReverseProxy{
 		Rewrite: func(r *httputil.ProxyRequest) {
-			fmt.Println(r)
+			fmt.Println(r.Out)
 			r.SetURL(target)
 			r.Out.Host = r.In.Host
+			r.SetXForwarded()
 		},
 	}
 
